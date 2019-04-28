6a7b5846b4b95ded8e9400baa2428b148cfa3373
jennifershih <cyshih55@gmail.com>
2019-04-28 15:35:43 -0700

    fix some minor thing

--- run_lasso.py
+++ run_lasso.py
@@ -5,16 +5,20 @@ import matplotlib.pyplot as plt
 A = np.random.randn(100, 500)
 b = np.random.randn(100, 1) * 25
 x0 = np.random.randn(500, 1)
-
 lamb = 4
 r = 5
-s = 0.01
+
+ATA = np.dot(A.T, A)
+
+
+# 1/L
+s = 0.1
 
 def f(x):
     return 0.5 * pow(np.linalg.norm(np.dot(A,x) - b), 2) + lamb * np.linalg.norm(x,ord=1)
 
 def df(x):
-    grad = np.dot(A,x) - b
+    grad = np.dot(A.T, np.dot(A,x) - b)
     tmp = np.zeros(x.shape)
     tmp[x<0] = -1.0
     tmp[x>0] = 1.0
