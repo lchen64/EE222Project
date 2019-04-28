7b09e1eb2f94e5a32ed39773db74995139a4297f
jennifershih <cyshih55@gmail.com>
2019-04-28 16:08:52 -0700

    use log for y axis

--- run_lasso.py
+++ run_lasso.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 A = np.random.randn(100, 500)
 b = np.random.randn(100, 1) * 25
-x0 = np.random.randn(500, 1)
+# x0 = np.random.randn(500, 1)
+x0 = np.ones((500, 1))
 lamb = 4
 r = 4
 
@@ -14,6 +15,9 @@ eig_1 = np.max(eigs)
 L = eig_1 + 2 * lamb
 
 s = 1.0 / L
+print(eig_1)
+# s = 0.000002
+print(s)
 
 def f(x):
     return 0.5 * pow(np.linalg.norm(np.dot(A,x) - b), 2) + lamb * np.linalg.norm(x,ord=1)
@@ -33,9 +37,15 @@ if __name__ == "__main__":
 
     xs = np.array(xs)
     fs = np.array(fs)
+    t = np.arange(len(xs))
+
+    k = 20
+    t = t[k:]
+    fs = fs[k:]
 
     plt.figure()
-    plt.plot(np.arange(len(xs)), fs)
+    plt.yscale('log',basey=10) 
+    plt.plot(t, fs, linewidth=1)
     plt.xlabel("iteration number")
     plt.ylabel("function value")
     plt.title("Nesterov's Lasso")
