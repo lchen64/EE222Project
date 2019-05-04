45763bb3d8ad1307ee0544542611c99f41fe7dc6
jennifershih <cyshih55@gmail.com>
2019-05-03 20:18:13 -0700

    working lasso 1

--- run_lasso.py
+++ run_lasso.py
@@ -1,22 +1,27 @@
 import numpy as np
 from nesterov import run_nesterov
 import matplotlib.pyplot as plt
+import cvxpy as cp
 
-A = np.random.randn(100, 500)
-b = np.random.randn(100, 1) * 25
-# x0 = np.random.randn(500, 1)
-x0 = np.ones((500, 1))
+# A = np.random.randn(500, 500)
+# b = np.random.randn(500, 1) * 3
+N = 20
+A = np.random.randn(N, N)
+b = np.random.randn(N, 1) * 0.3
+# x0 = np.ones((500, 1)) * 2
+# x0 = np.random.randn(500, 1) * 2
+x0 = np.random.uniform(low=-0.5, high=0.5, size=b.shape)
 lamb = 4
-r = 4
+r = 3
 
 ATA = np.dot(A.T, A)
 eigs, _ = np.linalg.eig(ATA)
 eig_1 = np.max(eigs)
-L = eig_1 + 2 * lamb
+L = eig_1 + lamb
 
-s = 1.0 / L
+# s = 1.0 / L
 print(eig_1)
-# s = 0.000002
+s = 0.00002
 print(s)
 
 def f(x):
@@ -31,15 +36,31 @@ def df(x):
 
     return grad + tmp
 
-if __name__ == "__main__":
+def objective_fn(X, Y, beta, lambd):
+    return 0.5 * cp.norm(cp.matmul(X, beta) - Y, 2)**2 + lambd * cp.norm(beta, 1)
 
+def f_star():
+    beta = cp.Variable(A.shape[1])
+    lambd = cp.Parameter(nonneg=True)
+    lambd.value = lamb
+    problem = cp.Problem(cp.Minimize(objective_fn(A, np.reshape(b, (len(b),)), beta, lambd)))
+
+    problem.solve()
+    beta = beta.value
+    beta = np.reshape(beta, (len(beta), 1))
+    return f(beta), beta
+
+if __name__ == "__main__":
+    f_opt, x_opt = f_star()
+    # x0 = x_opt + np.random.randn(x_opt.shape[0], x_opt.shape[1]) * 0.0001
     xs, fs = run_nesterov(f, df, x0, s, r, epsilon=pow(10, -3))
 
     xs = np.array(xs)
-    fs = np.array(fs)
+    print(f_opt)
+    fs = np.array(fs) - f_opt
     t = np.arange(len(xs))
 
-    k = 20
+    k = 0
     t = t[k:]
     fs = fs[k:]
 
